df4dc65ef85c9fdfde3118d1badea3507b21b0e4
lmonteagudo15 <lyanna.monteagudo@patelhs.org>
2020-09-24 17:54:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int myAge = 16;
+    int myBirthday = 923;
+    int todaysDate = 921;
+     if( myBirthday != todaysDate)
+     {
+        System.out.println("I am still " + myAge + " years old.");
+     }else{
+       System.out.println("i am " + myAge + "years old.");
+    }
+  } 
+} 
